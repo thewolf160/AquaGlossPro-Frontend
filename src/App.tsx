8bc103d45bf10d63e2efca24a5542a673542b8bf
FDmaster711 <fabiandacal7@gmail.com>
2026-04-17 19:44:48 -0400

    Sidebar modificada

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import ClientDisplay from "./pages/ClientHome";
 import ClientHeader from "./components/ClientHeader";
 import Clients from "./pages/Client";
 import ProtectedRoute from "./routes/ProtectedRoutes";
-
+import Reports from "./pages/Reports";
 
 export default function App() {
   return (
